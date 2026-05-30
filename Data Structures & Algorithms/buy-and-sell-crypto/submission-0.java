399bab2b80758fff357182f9dff51cf70ea6d9a6
Daria Toguleva <73368777+DariaToguleva@users.noreply.github.com>
2026-05-30 12:44:41 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       int max = 0; 
+
+       int i = 0;
+       int j = 1;
+       while (j < prices.length) {
+        if (prices[j] - prices[i] > max) {
+            max = prices[j] - prices[i];
+        }
+        if (prices[j] < prices[i]) {
+            i = j;
+        }
+        j++;
+       }
+       return max;
+    }
+}
